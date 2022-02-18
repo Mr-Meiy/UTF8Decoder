4a86f233b39532e33faa4dc303b6563e950e89dc
Mr-Meiy <54905082+Mr-Meiy@users.noreply.github.com>
2022-02-18 12:11:09 +0530

    Add files via upload
    Initial Commit

---
+++ UTF8Decoder.js
@@ -0,0 +1,36 @@
+const activateButton = () => {
+    const decodeButton = document.getElementById("decode")
+    const resultElement = document.getElementById("decodedResult")
+    const encodedInput = document.getElementById("encodedInput").value
+    if (encodedInput.length === 0) {
+        decodeButton.disabled = true;
+        resultElement.innerHTML = "";
+        resultElement.classList.remove("border");
+    } else {
+        decodeButton.disabled = false;
+    }
+}
+const decodeInput = () => {
+    const encodedInput = document.getElementById("encodedInput").value.replace('jsondata=', '')
+    const resultElement = document.getElementById("decodedResult")
+    try {
+        const decodedString = decodeURI(encodedInput).replaceAll('%3A', ':').replaceAll('%2C', ',');
+        console.log(decodedString);
+        resultElement.innerHTML = "UTF-8 Decoded String:";
+        try {
+            const decodedObject = JSON.parse(decodedString);
+            resultElement.innerHTML = JSON.stringify(JSON.parse(decodedString), null, "  ");
+        } catch (error) {
+            try {
+                resultElement.innerHTML = JSON.stringify(JSON.parse(decodedString));
+            } catch (err) {
+                console.log(err);
+                resultElement.innerHTML = err;
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        resultElement.innerHTML = error;
+    }
+    resultElement.classList.add("border");
+}
